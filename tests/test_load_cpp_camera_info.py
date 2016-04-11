abbb722260e750c66a18af4a05613e843b8c0b3d
Lucas Walter <wsacul@gmail.com>
2016-04-11 06:55:53 -0700

    #7 This lays out the beginning of a rsotest to generate and load a C++ generated camera_info into python, but not hooked up correctly yet, actual test part always succeeds

---
+++ tests/test_load_cpp_camera_info.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import unittest
+
+from camera_info_manager import *
+
+
+class TestLoadCppCameraInfo(unittest.TestCase):
+
+    def test_load_cpp_camera_info(self):
+        # TODO(lucasw) load the camera.yaml that the C++ camera_info_manager
+        # generated, then check values against expected.
+        # The values ought to be passed as args into both this test and the C++
+        # camera info generation.
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    rospy.init_node('test_load_cpp_camera_info')
+    import rostest
+    rostest.rosrun('camera_info_manager_py', 'test_load_cpp_camera_info', TestLoadCppCameraInfo)
